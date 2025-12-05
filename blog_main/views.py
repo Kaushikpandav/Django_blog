da0f9489e8882d9e1d3d08fcd0b5fc96ebe9de58
kaushikpandav <kaushikpandav16@gmail.com>
2025-12-05 11:25:26 +0530

    Attached reference.txt for upcomming code concept
    feat: Implement initial Django blog homepage with URL routing, views, template, and static assets.

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,16 @@
 
 from django.shortcuts import render
+from blogs.models import category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+    categories = category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    unfeatured_post = Blog.objects.filter(is_featured=False, status='Published').order_by('-updated_at')
+    print(unfeatured_post)
+    context = {
+        'categories': categories,
+        'featured_post': featured_post,
+        'unfeatured_post': unfeatured_post
+    }
+    return render(request, 'home.html', context)
 
