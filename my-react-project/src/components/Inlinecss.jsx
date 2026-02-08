1b1898082af9955f09a7af9e1177eda13c563c66
Kaushikpandav <kaushikpandav16@gmail.com>
2026-02-08 17:07:44 +0530

    internal and external CSS added to the component

--- my-react-project/src/components/Inlinecss.jsx
+++ my-react-project/src/components/Inlinecss.jsx
@@ -1,33 +1,54 @@
 import React from 'react'
+import '../assets/css/style.css'
 
 const Inlinecss = () => {
     const style = {
-        container:{
-            color:'red',
-            backgroundColor:'yellow',
+        container: {
+            color: 'red',
+            backgroundColor: 'yellow',
         },
-        paragraph:{
-            color:'blue',
-            backgroundColor:'pink',
+        paragraph: {
+            color: 'blue',
+            backgroundColor: 'pink',
         },
 
-        h2Text:{
-            color:'green',
-            backgroundColor:'orange',
-            textAlign:'center',
+        h2Text: {
+            color: 'green',
+            backgroundColor: 'orange',
+            textAlign: 'center',
         }
     }
-  return (
-    <>  
-    <h1 style={{color:'red',backgroundColor:'yellow'}}>This is heading</h1>
-    <p style={{color:'blue',backgroundColor:'pink'}}>This is paragraph</p>
+    return (
+        <>
+
+            <style>
+                {`
+                .Internal {
+                    color: purple;
+                    background-color: cyan;
+                    text-align: center;
+                    font-weight: bold;
+                    font-size: 20px;
+                    font-family: Arial, sans-serif;
+                }
+            `}
+
+            </style>
+            <h1 style={{ color: 'red', backgroundColor: 'yellow' }}>This is heading</h1>
+            <p style={{ color: 'blue', backgroundColor: 'pink' }}>This is paragraph</p>
 
 
-    <div style={style.container}>
-        <h2 style={style.h2Text}>This is h2 text</h2>
-    </div>
-    </>
-  )
+            <div style={style.container}>
+                <h2 style={style.h2Text}>This is h2 text</h2>
+            </div>
+            <hr />
+
+            <div className='Internal'>it's experiment</div>
+
+            <h1>It's external CSS</h1>
+            <div className="Bluebox">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptates laboriosam eaque, repellat adipisci sequi. Expedita alias, magnam fugiat placeat, temporibus quas possimus obcaecati provident ab, cum repellendus ullam nemo.</div>
+        </>
+    )
 }
 
 export default Inlinecss
