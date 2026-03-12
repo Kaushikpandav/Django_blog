d202b143387b7a2ab083a4e7d3dfc60e651760bd
kaushik pandav <kaushik@benzatine.com>
2026-03-12 12:17:04 +0530

    feat(caching): Add view caching and extensive notes
    - Implement `@cache_page` decorator on views
    - Introduce new `home` and `about` views
    - Comment out original session views
    
    cache in details...

--- ch41/student/urls.py
+++ ch41/student/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import setsession, getsession, deletesession, flushsession, sessionmethodsinview, sessionmethodsintemplate, clearsession, settestcookie, checktestcookie, deltestcookie
-
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
   # path('set/', setcookie, name='setcookie'),
@@ -10,8 +10,11 @@ urlpatterns = [
   # path('getsigned/', getsignedcookie, name='getsignedcookie'),
 
   path('set/', setsession, name='setsession'),
+  path('setss/', cache_page(60)(setsession), name='setsession'),
+  path('setcc/', setsession, name='setsession'),
+
   path('get/', getsession, name='getsession'),
-  path('del/', deletesession, name='deletesession'),
+  path('del/', cache_page(60)(deletesession), name='deletesession'),
   path('flush/', flushsession, name='flushsession'),
   path('inview/', sessionmethodsinview, name='inview'),
   path('intemplate/', sessionmethodsintemplate, name='intemplate'),
--- ch41/student/views.py
+++ ch41/student/views.py
@@ -1,6 +1,6 @@
 import datetime
 from django.shortcuts import render
-
+from django.views.decorators.cache import cache_page
 
 
 #cookie
@@ -71,92 +71,100 @@ from django.shortcuts import render
 
 
 #session
-def setsession(request):
-  request.session['token'] = f'{datetime.datetime.now()}'
-  request.session['name'] = 'Django'
-  # request.session.set_expiry(60)
-  # request.session.set_expiry(0) # expiry at browser close
+# def setsession(request):
+#   request.session['token'] = f'{datetime.datetime.now()}'
+#   request.session['name'] = 'Django'
+#   # request.session.set_expiry(60)
+#   # request.session.set_expiry(0) # expiry at browser close
 
-  return render(request, 'student/setsession.html')
+#   return render(request, 'student/setsession.html')
 
-def getsession(request):
-  token = request.session['token']
-  name = request.session['name']
+# def getsession(request):
+#   token = request.session['token']
+#   name = request.session['name']
 
-  ses = {
-    'token': token,
-    'name': name
-  }
-  return render(request, 'student/getsession.html', ses)
+#   ses = {
+#     'token': token,
+#     'name': name
+#   }
+#   return render(request, 'student/getsession.html', ses)
 
-def deletesession(request):
-  if 'name' in request.session:
-    del request.session['token']
-  # del request.session['name']
-  return render(request, 'student/deletesession.html')
+# def deletesession(request):
+#   if 'name' in request.session:
+#     del request.session['token']
+#   # del request.session['name']
+#   return render(request, 'student/deletesession.html')
 
-def flushsession(request):
-  request.session.flush()
-  return render(request, 'student/flushsession.html')
+# def flushsession(request):
+#   request.session.flush()
+#   return render(request, 'student/flushsession.html')
 
-def sessionmethodsinview(request):
-  keys = request.session.keys()
-  print(keys)
+# def sessionmethodsinview(request):
+#   keys = request.session.keys()
+#   print(keys)
 
-  for key, value in request.session.items():
-    print(key, value)
+#   for key, value in request.session.items():
+#     print(key, value)
 
-  age = request.session.setdefault('age',21)
-  print(age)
+#   age = request.session.setdefault('age',21)
+#   print(age)
 
-  session_age = request.session.get_session_cookie_age()
-  print("session cookie age: ", session_age)
+#   session_age = request.session.get_session_cookie_age()
+#   print("session cookie age: ", session_age)
 
-  expiry_date = request.session.get_expiry_date()
-  print("expiry date: ", expiry_date)
+#   expiry_date = request.session.get_expiry_date()
+#   print("expiry date: ", expiry_date)
 
-  expiry_age = request.session.get_expiry_age()
-  print("expiry age: ", expiry_age)
+#   expiry_age = request.session.get_expiry_age()
+#   print("expiry age: ", expiry_age)
 
-  expiry_at_browser_close = request.session.get_expire_at_browser_close()
-  print("browser close: ", expiry_at_browser_close)
+#   expiry_at_browser_close = request.session.get_expire_at_browser_close()
+#   print("browser close: ", expiry_at_browser_close)
 
-  request.session['token'] = f'{datetime.datetime.now()}'
-  request.session['name'] = 'Django'
-  return render(request, 'student/sessionmethodsinview.html')
-
-
-def clearsession(request):
-  request.session.clear_expired()
-  return render(request, 'student/clearsession.html')
+#   request.session['token'] = f'{datetime.datetime.now()}'
+#   request.session['name'] = 'Django'
+#   return render(request, 'student/sessionmethodsinview.html')
 
 
-def sessionmethodsintemplate(request):
-  for key, value in request.session.items():
-    print(key, value)
-    value = {
-      'key': key,
-      'value': value
-    }
-  return render(request, 'student/sessionmethodsintemplate.html', value)
-
-def settestcookie(request):
-  response = render(request, 'student/settestcookie.html')
-  request.session.set_test_cookie()
-  return response
-
-def checktestcookie(request):
-  print(request.session.test_cookie_worked()) # true or false
-  return render(request, 'student/checktestcookie.html')
-
-def deltestcookie(request):
-  response = render(request, 'student/deltestcookie.html')
-  request.session.delete_test_cookie()
-  return response
+# def clearsession(request):
+#   request.session.clear_expired()
+#   return render(request, 'student/clearsession.html')
 
 
-# in seeting
+# def sessionmethodsintemplate(request):
+#   for key, value in request.session.items():
+#     print(key, value)
+#     value = {
+#       'key': key,
+#       'value': value
+#     }
+#   return render(request, 'student/sessionmethodsintemplate.html', value)
 
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 7
-SESSION_COOKIE_NAME = 'sessionid'
-SESSION_COOKIE_PATH = '/home'
+# def settestcookie(request):
+#   response = render(request, 'student/settestcookie.html')
+#   request.session.set_test_cookie()
+#   return response
+
+# def checktestcookie(request):
+#   print(request.session.test_cookie_worked()) # true or false
+#   return render(request, 'student/checktestcookie.html')
+
+# def deltestcookie(request):
+#   response = render(request, 'student/deltestcookie.html')
+#   request.session.delete_test_cookie()
+#   return response
+
+
+# # in seeting
+
+# SESSION_COOKIE_AGE = 60 * 60 * 24 * 7
+# SESSION_COOKIE_NAME = 'sessionid'
+# SESSION_COOKIE_PATH = '/home'
+
+
+@cache_page(timeout = 60 * 15, key_prefix='test')
+def home(request):
+  return render(request, 'student/home.html')
+
+def about(request):
+  return render(request, 'student/about.html')
