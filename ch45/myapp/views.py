535027cb6c69d0fd7018341319242b06d48a955b
kaushik pandav <kaushik@benzatine.com>
2026-03-17 15:21:23 +0530

    feat(ch45): add async/sync view example
    - Set up new Django project and app
    - Implement synchronous and asynchronous views
    - Demonstrate HTTP requests with httpx
    - Update notes with

---
+++ ch45/myapp/views.py
@@ -0,0 +1,56 @@
+import asyncio
+import time
+
+from django.shortcuts import HttpResponse
+import httpx
+from django.http import JsonResponse
+
+# def index(request):
+#   # this is general view and execute syncronously
+#   return HttpResponse("hello")
+
+# async def task1(request):
+#   # this is general view and execute syncronously
+#   return HttpResponse("hello")
+
+def sync_view(request):
+  start = time.time()
+
+  responses = []
+
+  for _ in range(10):
+    res = httpx.get("https://google.com")
+    responses.append(res.json())
+    print(time.time() - start)
+
+  return JsonResponse(
+    {
+      "responses": responses,
+      "time taken": time.time() - start
+    }
+)
+
+async def async_view(request):
+  start = time.time()
+
+  async with httpx.AsyncClient() as client:
+    # task = [client.get("https://google.com") for _ in range(10)]
+    # responses = await asyncio.gather(*task)
+    task1 = client.get("https://google.com")
+    task2 = client.get("https://google.com")
+    task3 = client.get("https://google.com")
+    task4 = client.get("https://google.com")
+    task5 = client.get("https://google.com")
+    task6 = client.get("https://google.com")
+    task7 = client.get("https://google.com")
+    task8 = client.get("https://google.com")
+    task9 = client.get("https://google.com")
+    task10 = client.get("https://google.com")
+    responses = await asyncio.gather(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10)
+
+  return JsonResponse(
+    {
+      "responses": responses,
+      "time taken": time.time() - start
+    }
+  )
